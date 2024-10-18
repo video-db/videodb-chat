ab12d8faf6ed762c06801b321b1d98239f1b2474
0xrohitgarg <rohit@spext.co>
2024-10-18 17:14:18 +0530

    edits

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 import type { App } from "vue";
 import ChatInterface from "./components/chat/ChatInterface.vue";
-import { useChatInterface } from "./components/hooks/useChatInterface";
+import { useVideoDBChat } from "./context";
 import "./style.css";
 
 function install(app: App) {
   app.component("ChatInterface", ChatInterface);
 }
 
-export {
-  install,
-  ChatInterface,
-  useChatInterface
-};
+export { install, ChatInterface, useVideoDBChat };
